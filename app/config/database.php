1e9eb7250acab1dc4aa2f9dc6fd8640721b9c3a2
Miguel Mendez <keloxers@gmail.com>
2014-11-04 19:59:17 -0300

    upoasd

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'cooper',
+			'database'  => 'vvirtual',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
